c2b2baa255f2fb3ca8d3d7445d74e7205492a38a
vasavi-s <81638871+vasavi-s@users.noreply.github.com>
2021-06-02 16:11:41 +0530

    Delete main.py

--- Day-36/main.py
+++
@@ -1,9 +0,0 @@
-# Day 36 - Stock Trading News Alert Project
-Normal Difficulty Starting Project
-Hard Difficulty Starting Project
-Extra Hard Difficulty Starting Project
-TSLA STock Price
-Stock Price API
-News API
-Twilio API
-Completed Code for Stock Trading Alert Project
