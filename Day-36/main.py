8232881f6de1459288e3807d85b79674ededa434
vasavi-s <81638871+vasavi-s@users.noreply.github.com>
2021-06-02 16:06:57 +0530

    Create main.py

---
+++ Day-36/main.py
@@ -0,0 +1,9 @@
+# Day 36 - Stock Trading News Alert Project
+Normal Difficulty Starting Project
+Hard Difficulty Starting Project
+Extra Hard Difficulty Starting Project
+TSLA STock Price
+Stock Price API
+News API
+Twilio API
+Completed Code for Stock Trading Alert Project
